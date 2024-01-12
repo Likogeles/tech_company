16732d855991962ce18f9b1ed745df0453219ac9
Likogeles <57628609+Likogeles@users.noreply.github.com>
2024-01-12 09:33:48 +0400

    Добавлен локальный сервер

--- lib/app_design.dart
+++ lib/app_design.dart
@@ -11,4 +11,5 @@ const List<String> categoryList = ["Общее", "Личное"];
 const List<String> techStateList = ["Работает", "Требуется ремонт", "В ремонте", "Неисправно"];
 const List<String> departmentList = ["Отдел кадров", "Охрана", "Отдел продаж", "Технический отдел"];
 
-const String url = "http://176.57.218.12:5000";
+// const String url = "http://176.57.218.12:5000";
+const String url = "http://10.0.2.2:5000";
